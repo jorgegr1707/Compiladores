9427eea59cb88e1be0b864f77b94a2cbc51fff16
jorgegr1707 <jorgegr1707@gmail.com>
2017-11-13 08:06:51 -0600

    Actualizacion de preprocesador

--- ProyectoCompiladores3/Pruebas/hola.c
+++ ProyectoCompiladores3/Pruebas/hola.c
@@ -1,19 +1,6 @@
-#define putada 22
-#define puta 340
-#define tome puta
-
-
+#include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char** argv){
 	int colacho = 1;
-	if(colacho == tome){
-		return tome;	
-	}
-	else{
-		printf("Not equal");
-	}
-
-	while(colacho != 10){
-		colacho++
-	}
-	
+	return colacho;	
 }
